00af60ad6e84f06424c5b7f34b6ec4eace339534
B3none <alexblackham@silktide.com>
2018-01-29 14:24:04 +0000

    Moved the index.js into a src folder. Also made it so that we write the domains.txt to an output folder. Also made it so that if the folder does not exist we create it.

--- src/index.js
+++ src/index.js
@@ -2,33 +2,40 @@ var request = require('request');
 var unzip = require('unzip');
 var csv2 = require('csv2');
 var fs = require('fs');
+var mkdirp = require('mkdirp');
+
 
 var domains = [];
 
-request
-  .get('http://s3.amazonaws.com/alexa-static/top-1m.csv.zip')
-  .pipe(unzip.Parse())
-  .on('entry', function(entry) {
-    entry
-      .pipe(csv2())
-      .on('data', function(data) {
-        var progress = Math.round(parseInt(data[0], 10) / 10000);
-        domains.push(data[1]);
-        process.stdout.write('Progress: ' + progress + '%\r');
-      })
-      .on('end', function() {
-        var file = fs.createWriteStream('output/domains.txt');
+mkdirp('output', function (err) {
+  if (err) {
+    return console.error(err);
+  }
+  request
+    .get('http://s3.amazonaws.com/alexa-static/top-1m.csv.zip')
+    .pipe(unzip.Parse())
+    .on('entry', function(entry) {
+      entry
+        .pipe(csv2())
+        .on('data', function(data) {
+          var progress = Math.round(parseInt(data[0], 10) / 10000);
+          domains.push(data[1]);
+          process.stdout.write('Progress: ' + progress + '%\r');
+        })
+        .on('end', function() {
+          var file = fs.createWriteStream('output/domains.txt');
 
-        domains.sort();
+          domains.sort();
 
-        file.on('error', function(err) {
-          console.error(err);
+          file.on('error', function(err) {
+            console.error(err);
+          });
+
+          domains.forEach(function(domain) {
+            file.write(domain + '\n');
+          });
+
+          file.end();
         });
-
-        domains.forEach(function(domain) {
-          file.write(domain + '\n');
-        });
-
-        file.end();
-      });
-  });
+    });
+});
